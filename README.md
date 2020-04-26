1bdf643eaf9687f0f2c63290ea188dea3106c0bb
lamine1305 <lamine1305@gmail.com>
2020-04-26 10:50:58 +0200

    commit du projet

---
+++ README.md
@@ -0,0 +1,1 @@
+# projectForJenkins est un projet servant pour les tests maven sous jenkisn
---
+++ src/main/java/Prinipale.java
@@ -0,0 +1,8 @@
+public class Prinipale {
+
+    public static void main (String[]arg){
+
+        System.out.println("ceci est un projet maven pour test jenins ...");
+
+    }
+}
